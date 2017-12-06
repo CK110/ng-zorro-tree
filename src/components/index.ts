bb524e50912bf271d5945436bcce8d590529ba83
CK <chenkai4dev@163.com>
2017-12-06 17:32:46 +0800

    增加导出

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export {NzTreeModule} from './tree/nz-tree.module';
 export {NzTreeComponent} from './tree/nz-tree.component';
+export {NzTreeSelectModule} from './tree-select/nz-tree-select.module';
+export {NzTreeSelectComponent} from './tree-select/nz-tree-select.component';
 
